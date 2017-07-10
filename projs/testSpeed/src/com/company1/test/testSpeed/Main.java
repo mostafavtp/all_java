d447306ea0014940e2ad843f0c90601f5ea07d0d
mostafa_vtp <mostafa_vtp@yahoo.com>
2017-07-10 18:27:24 +0430

    first proj

---
+++ projs/testSpeed/src/com/company1/test/testSpeed/Main.java
@@ -0,0 +1,11 @@
+package com.company1.test.testSpeed;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
